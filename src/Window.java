313e703600690d472973f1ac4bcee552b29a3ab4
Rishi Kumar <rishikumar10dec@gmail.com>
2024-02-28 17:29:31 +0530

    keyListener is added

--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
     Graphics2D g2;
+    KListener keyListener = new KListener();
 
     public Window() {
         this.setSize(Constents.SCREEN_WIDTH, Constents.SCREEN_HEIGHT);
@@ -11,14 +13,19 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
         g2 = (Graphics2D) this.getGraphics();
     }
 
     public void update(double dt) {
-        System.out.println(dt + " seconds passed since last frame");
-        System.out.println(1 / dt + " FPS");
         g2.setColor(Color.BLACK);
         g2.fillRect(0, 0, Constents.SCREEN_WIDTH, Constents.SCREEN_HEIGHT);
+
+        if(keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("UP key is pressed");
+        } else if(keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("DOWN key is down");
+        }
     }
 
     public void run() {
